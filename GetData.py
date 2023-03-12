f122c73f6a6ffc74de91f0743910d854686fdbd2
jonathan122 <jonathan.christipherw@gmail.com>
2023-03-12 00:53:47 +0000

    Adding delete_old_files() function that deletes hourly data files that are any older than of last month

--- GetData.py
+++ GetData.py
@@ -1,9 +1,55 @@
 # Importing necessary modules
 from binance_historical_data import BinanceDataDumper
+import os
 import datetime
+
 
 #todays date
 today = datetime.date.today()
+
+
+def delete_old_files():
+    # Set the directory where the files are saved
+    data_dir = r'C:\Users\jonat\Documents\crypto\spot2\spot\monthly\klines\SOLUSDT\1h'
+    
+    # Get the current month and last month
+    today = datetime.date.today()
+    current_month = today.month
+    last_month = (today.replace(day=1) - datetime.timedelta(days=1)).month
+
+    # Initialize a counter to track the number of files deleted
+    num_files_deleted = 0
+
+    # Loop through the files in the data directory
+    for filename in os.listdir(data_dir):
+        # Check if the file name matches the format SOLUSDT-1h-YYYY-MM
+        if not filename.startswith('SOLUSDT-1h-'):
+            continue
+        
+        try:
+            # Extract the date from the file name
+            file_date_str = filename.split('-')[2] + '-' + filename.split('-')[3]
+            file_date = datetime.datetime.strptime(file_date_str, '%Y-%m').date()
+        except (IndexError, ValueError):
+            # Skip files with invalid date formats
+            continue
+
+        # Check if the file date is from the current or last month
+        if file_date.month not in [current_month, last_month]:
+            # Delete the file
+            os.remove(os.path.join(data_dir, filename))
+            print(f"Deleted file: {filename}")
+            num_files_deleted += 1
+
+    # Print the number of files deleted
+    if num_files_deleted == 0:
+        print("No files were deleted.")
+    else:
+        print(f"Deleted {num_files_deleted} files.")
+
+
+
+
 
 # Creating an instance of BinanceDataDumper class
 # Setting the path to the directory where dumped data will be saved,
@@ -64,4 +110,4 @@ data_dumper2.dump_data(
 data_dumper.delete_outdated_daily_results()
 
 #Deleting any outdated hourly results that aren't needed.
-data_dumper2.delete_outdated_daily_results()
+delete_old_files()
